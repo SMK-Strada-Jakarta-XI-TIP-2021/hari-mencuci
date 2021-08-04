19cab44146b70d24fd108882382805b15dfc3fa5
novi <novia@gmail.com>
2021-08-04 21:28:50 +0700

    tugas hari-mencuci novi

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
 // Tulis Kode Kamu di bawah ini
+function mesinCuci(nama, jmlhpakaian) {
+    for (let x = 1; x <= jmlhpakaian; x++) {
+        console.log(nama + ' sudah memsukan ' + x + ' lembar pakaian.');
+        if (x === jmlhpakaian) {
+            console.log('ini sudah pakaian terakhir power mesin cuci akan otomatis menyala.');
+            break;
+        }
+    }
+}
+
+mesinCuci("Novia", 30);
