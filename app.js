d26573106bb90c3454b6defd22cfe01af3c390d7
unknown <dwitegar611@gmail.com>
2021-08-04 23:21:58 +0700

    perubahan pertama

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
 // Tulis Kode Kamu di bawah ini
+function mesinCuci(nama, jmlhpakaian) {
+    for (let x = 1; x <= jmlhpakaian; x++) {
+        console.log(nama + ' sedang memasukan pakaian sebanyak ' + x + ' lembar.');
+        if (x === jmlhpakaian) {
+            console.log('ini sudah pakaian terakhir dan power mesin cuci akan otomatis menyala.');
+        }
+    }
+}
+
+mesinCuci("tegar", 30);
