4a3df3bcb5e346b5adbb15ab6aa82a4d60770460
adrigunawan <adrigunawan32@gmail.com>
2021-08-06 00:35:17 +0700

    Tugas hari mencuci

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
 // Tulis Kode Kamu di bawah ini
+function mesinCuci (nama, jumlahBaju) {
+    for (i = 1; i <= jumlahBaju; i++) {
+        console.log(nama + " " + 'Anda sudah memasukan' + " " + jumlahBaju + " " + 'lembar pakaian');
+        if (i === jumlahBaju) {
+            console.log ("mesin cuci terhenti !");
+            break;
+        }
+    }   
+}
+
+mesinCuci("Adri",10);
