b5618fae3d69c357e59461d9ce788cccc3f01a31
sulthanarif <msarizky201@gmail.com>
2021-08-07 19:37:08 +0700

    sulthan

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
 // Tulis Kode Kamu di bawah ini
+function nyuci(nama,baju) {
+    for(var x=1; x <= baju; x++) {
+        console.log('Pakaian yang dicuci oleh ' + nama + ' sebanyak ' + x + ' lembar dan dimasukkan ke dalam mesin cuci.');
+        if (x === baju) {
+            console.log('Tombol power mesin cuci akan otomatis menyala jika semua pakaian ' + nama + ' sudah masuk ke dalam mesin cuci.');
+        }
+    }
+}
+nyuci("sulthan",50);
