abb4d6ae7fe3f787116c7bca96c16dd5907c031c
myairin <irenewindagultom@gmail.com>
2021-08-13 13:15:49 +0700

    revisi hari mencuci

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
 // Tulis Kode Kamu di bawah ini
+
+function mesinCuci(nama, jumlahBaju) {
+    for (let x  = 1; x <= jumlahBaju; x++) {
+        console.log(nama + ' ' + 'memasukan pakaian sebanyak' + ' ' + jumlahBaju + ' ' + 'lembar')
+        if (x === jumlahBaju) {
+            console.log('Tombol power mesin cuci akan otomatis menyala jika semua pakaian Novia sudah masuk ke mesin cuci')
+        }
+    }
+}
+
+
+mesinCuci('Novia', 30);
+
+
+
