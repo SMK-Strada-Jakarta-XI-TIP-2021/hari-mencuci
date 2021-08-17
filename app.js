6777696192f88001f64d76e13b53b3075f6646b8
zkarine <zefanyakarine03@gmail.com>
2021-08-16 18:40:31 -0700

    zef

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
 // Tulis Kode Kamu di bawah ini
+
+function laundry(nama, pakaian){
+    for (let x = 1; x <= pakaian; x++) {
+        console.log(nama + 'memasukan pakaian sebanyak' + x + 'lembar')
+        if (x === pakaian) {
+            console.log('Tombol power mesin cuci akan otomatis menyala jika semua pakaian Novia sudah masuk ke mesin cuci.');
+        }
+    }
+}
+
+laundry("novia", 30); 
